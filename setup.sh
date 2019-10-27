95de948d4ec8f99b0a35af3eed90f59ff81d9be0
Brian Yu <brian@byu.io>
2019-10-27 19:02:23 -0400

    Setup

---
+++ setup.sh
@@ -0,0 +1,11 @@
+
+
+sudo snap install docker
+sudo setfacl -m user:$USER:rw /var/run/docker.sock
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+docker build -t daemon .
