4aec7a328416db549decb91df571c445563ed559
Brian Yu <brian@byu.io>
2019-10-29 12:11:47 -0400

    Attempt to limit PS GPU usage

--- task2.py
+++ task2.py
@@ -32,6 +32,23 @@ def create_folder_get_path(relative_path):
     return path
 
 def main(_):
+
+    ## Attempt to allocate only as much memory as needed on PS.
+    if FLAGS.job_name == "ps":
+        gpus = tf.config.experimental.list_physical_devices('GPU')
+        if gpus:
+          try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+              tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+          except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+
+
+
     ps_hosts = FLAGS.ps_hosts.split(",")
     worker_hosts = FLAGS.worker_hosts.split(",")
     # train_folder = FLAGS.train_folder
