28a9fb78a682c737663c20d89a569c2098724fa5
Brian Yu <brian@byu.io>
2019-11-01 14:36:12 -0400

    Add LSTM text generation

--- jobs/lstm_text_generation.py
+++ jobs/lstm_text_generation.py
@@ -34,7 +34,7 @@ def main(args):
 
     job_name = args.job
     checkpoint_dir = f"checkpoints/{job_name}"
-
+    logger = Logger(job_name)
 
     path = get_file(
         'nietzsche.txt',
@@ -77,47 +77,47 @@ def main(args):
     model.compile(loss='categorical_crossentropy', optimizer=optimizer)
 
 
-    def sample(preds, temperature=1.0):
-        # helper function to sample an index from a probability array
-        preds = np.asarray(preds).astype('float64')
-        preds = np.log(preds) / temperature
-        exp_preds = np.exp(preds)
-        preds = exp_preds / np.sum(exp_preds)
-        probas = np.random.multinomial(1, preds, 1)
-        return np.argmax(probas)
+    # def sample(preds, temperature=1.0):
+    #     # helper function to sample an index from a probability array
+    #     preds = np.asarray(preds).astype('float64')
+    #     preds = np.log(preds) / temperature
+    #     exp_preds = np.exp(preds)
+    #     preds = exp_preds / np.sum(exp_preds)
+    #     probas = np.random.multinomial(1, preds, 1)
+    #     return np.argmax(probas)
 
 
-    def on_epoch_end(epoch, _):
-        # Function invoked at end of each epoch. Prints generated text.
-        print()
-        print('----- Generating text after Epoch: %d' % epoch)
+    # def on_epoch_end(epoch, _):
+    #     # Function invoked at end of each epoch. Prints generated text.
+    #     print()
+    #     print('----- Generating text after Epoch: %d' % epoch)
 
-        start_index = random.randint(0, len(text) - maxlen - 1)
-        for diversity in [0.2, 0.5, 1.0, 1.2]:
-            print('----- diversity:', diversity)
+    #     start_index = random.randint(0, len(text) - maxlen - 1)
+    #     for diversity in [0.2, 0.5, 1.0, 1.2]:
+    #         print('----- diversity:', diversity)
 
-            generated = ''
-            sentence = text[start_index: start_index + maxlen]
-            generated += sentence
-            print('----- Generating with seed: "' + sentence + '"')
-            sys.stdout.write(generated)
+    #         generated = ''
+    #         sentence = text[start_index: start_index + maxlen]
+    #         generated += sentence
+    #         print('----- Generating with seed: "' + sentence + '"')
+    #         sys.stdout.write(generated)
 
-            for i in range(400):
-                x_pred = np.zeros((1, maxlen, len(chars)))
-                for t, char in enumerate(sentence):
-                    x_pred[0, t, char_indices[char]] = 1.
+    #         for i in range(400):
+    #             x_pred = np.zeros((1, maxlen, len(chars)))
+    #             for t, char in enumerate(sentence):
+    #                 x_pred[0, t, char_indices[char]] = 1.
 
-                preds = model.predict(x_pred, verbose=0)[0]
-                next_index = sample(preds, diversity)
-                next_char = indices_char[next_index]
+    #             preds = model.predict(x_pred, verbose=0)[0]
+    #             next_index = sample(preds, diversity)
+    #             next_char = indices_char[next_index]
 
-                sentence = sentence[1:] + next_char
+    #             sentence = sentence[1:] + next_char
 
-                sys.stdout.write(next_char)
-                sys.stdout.flush()
-            print()
+    #             sys.stdout.write(next_char)
+    #             sys.stdout.flush()
+    #         print()
 
-    print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+    # print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
 
 
     ckpt = os.path.join(checkpoint_dir, f"latest_model_{job_name}.h5")
@@ -125,10 +125,14 @@ def main(args):
         print(f"Restoring model from {ckpt}")
         model = load_model(ckpt)
 
+    # history = model.fit(x, y,
+    #           batch_size=128,
+    #           epochs=1, #60,
+    #           callbacks=[print_callback])
+
     history = model.fit(x, y,
               batch_size=128,
-              epochs=1, #60,
-              callbacks=[print_callback])
+              epochs=1)
 
     print(history.history.keys())
 
